fb14e3986786550bd6f70d3d5543d03f22c4fee5
Jacob Men <jacobmen416@yahoo.com>
2021-11-27 13:00:29 -0600

    Fixed program reading

--- src/bf.rs
+++ src/bf.rs
@@ -1,3 +1,6 @@
+use std::io::Read;
+use std::str;
+
 /// Enum representing commands found here: https://en.wikipedia.org/wiki/Brainfuck#Commands
 #[derive(PartialEq)]
 enum Instruction {
@@ -7,26 +10,44 @@ enum Instruction {
     DecrementByte,
     OutputByte,
     ReadByte,
-    ConditionalOpen,
-    ConditionalClose,
+    ConditionalOpen(usize),
+    ConditionalClose(usize),
     Ignore,
+}
+
+impl Instruction {
+    /// Returns `true` if the instruction is [`ConditionalOpen`].
+    ///
+    /// [`ConditionalOpen`]: Instruction::ConditionalOpen
+    fn is_conditional_open(&self) -> bool {
+        matches!(self, Self::ConditionalOpen(..))
+    }
+
+    /// Returns `true` if the instruction is [`ConditionalClose`].
+    ///
+    /// [`ConditionalClose`]: Instruction::ConditionalClose
+    fn is_conditional_close(&self) -> bool {
+        matches!(self, Self::ConditionalClose(..))
+    }
 }
 
 const DATA_SIZE: usize = 30_000;
 
 /// Interpreter for bf based on: https://en.wikipedia.org/wiki/Brainfuck
-pub struct BFInterpreter {
+pub struct Interpreter {
     instructions: Vec<Instruction>,
     data: [i8; DATA_SIZE],
     instr_ptr: usize,
     data_ptr: usize,
-    // TODO: Add fields for input and output streams to use with '.' and ','
+    terminated: bool,
 }
 
-impl BFInterpreter {
+impl Interpreter {
     // TODO: Return Result after implementing checks for matching parenthesis
     pub fn new(program: &str) -> Self {
         let mut instructions = Vec::with_capacity(program.len());
+        let mut matching_pair_indices = Vec::new();
+
         for char in program.chars() {
             let instruction = match char {
                 '>' => Instruction::IncrementDP,
@@ -35,8 +56,15 @@ impl BFInterpreter {
                 '-' => Instruction::DecrementByte,
                 '.' => Instruction::OutputByte,
                 ',' => Instruction::ReadByte,
-                '[' => Instruction::ConditionalOpen,
-                ']' => Instruction::ConditionalClose,
+                '[' => {
+                    matching_pair_indices.push(instructions.len());
+                    Instruction::ConditionalOpen(0)
+                }
+                ']' => {
+                    let opening_pair = matching_pair_indices.pop().expect("shouldn't be empty");
+                    instructions[opening_pair] = Instruction::ConditionalOpen(instructions.len());
+                    Instruction::ConditionalClose(opening_pair)
+                }
                 _ => Instruction::Ignore,
             };
 
@@ -45,11 +73,17 @@ impl BFInterpreter {
             }
         }
 
+        assert!(matching_pair_indices.is_empty());
+
         Self {
             instructions,
             data: [0; DATA_SIZE],
             instr_ptr: 0,
             data_ptr: 0,
+            terminated: false,
+        }
+    }
+
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod bf;
 
-use bf::BFInterpreter;
+use bf::Interpreter;
 use std::fs;
 
 fn main() {
     // TODO: read file passed as argument
-    let file_content = fs::read_to_string("test.bf").expect("couldn't read file");
-    let interpreter = BFInterpreter::new(&file_content);
+    let file_content = fs::read_to_string("./data/test.bf").expect("couldn't read file");
+    let mut interpreter = Interpreter::new(&file_content);
+    interpreter.run();
 }
