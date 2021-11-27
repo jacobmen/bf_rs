64a1adda95f627b4448be0a94ab54b2ab3772105
Jacob Men <jacobmen416@yahoo.com>
2021-11-27 15:42:01 -0600

    implement reading file from cli args

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 mod bf;
 
 use bf::Interpreter;
+use std::env;
 use std::fs;
+use std::process::exit;
 
 fn main() {
-    // TODO: read file passed as argument
-    let file_content = fs::read_to_string("./data/test.bf").expect("couldn't read file");
+    let mut args = env::args();
+    let process_name = args.next().expect("process name argument should exist");
+    let input_file = args.next().unwrap_or_else(|| {
+        eprintln!("Usage: {} <file_name>", process_name);
+        exit(1);
+    });
+    let file_content = fs::read_to_string(&input_file).unwrap_or_else(|_| {
+        eprintln!("[ERROR] Couldn't read input file: {}", input_file);
+        exit(1);
+    });
+
     let mut interpreter = Interpreter::new(&file_content);
     interpreter.run();
 }
